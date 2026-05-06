bf9a882492ebc3ebc9625f19f6ccb2c9049fe711
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-06 09:51:17 +0530

    Add files via upload

---
+++ Dbinfo.java
@@ -0,0 +1,9 @@
+package com.pack;
+
+public interface Dbinfo 
+{
+	 String url="jdbc:oracle:thin:@localhost:1521:orcl";
+	  String driver ="oracle.jdbc.OracleDriver";
+	  String Uname = "System";
+	  String pwd = "Raviraj1710";
+}
