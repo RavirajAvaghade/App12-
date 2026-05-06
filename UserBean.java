d64a8385a5cae7e199fe228e2ac6b8165a8ec6cd
RavirajAvaghade <ravirajavaghade508@gmail.com>
2026-05-06 09:51:47 +0530

    Add files via upload

---
+++ UserBean.java
@@ -0,0 +1,68 @@
+package com.pack;
+
+public class UserBean 
+{
+   String u_username,u_password,u_firstname,u_lastname,u_mailid,u_phone;
+
+   public String getU_username() 
+   {
+	return u_username;
+   }
+
+   public void setU_username(String u_username) 
+   {
+	this.u_username = u_username;
+   }
+
+   public String getU_password() 
+   {
+	return u_password;
+   }
+
+   public void setU_password(String u_password) 
+   {
+	this.u_password = u_password;
+   }
+
+   public String getU_firstname() 
+   {
+	return u_firstname;
+   }
+
+   public void setU_firstname(String u_firstname)
+   {
+	this.u_firstname = u_firstname;
+   }
+
+   public String getU_lastname() 
+   {
+	return u_lastname;
+   }
+
+   public void setU_lastname(String u_lastname)
+   {
+	this.u_lastname = u_lastname;
+   }
+
+   public String getU_mailid() 
+   {
+	return u_mailid;
+   }
+
+   public void setU_mailid(String u_mailid) 
+   {
+	this.u_mailid = u_mailid;
+   }
+
+   public String getU_phone() 
+   {
+	return u_phone;
+   }
+
+  
+
+   public void setU_phone(String u_phone) 
+   {
+	  this.u_phone = u_phone;
+   }
+}
